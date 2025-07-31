b43dbb37685131b13afa973011b0cf3309895545
yavorsk <yavor.krastev@sitecore.com>
2025-07-31 14:16:30 +0300

    add sites config file

---
+++ sites.ts
@@ -0,0 +1,14 @@
+export const sites = [
+  {
+    id: "siteA",
+    domains: ["localhost:3000"],
+    locales: ["en", "de-DE"],
+    defaultLocale: "en",
+  },
+  {
+    id: "siteB",
+    domains: ["www.siteB.com", "siteB.com"],
+    locales: ["fr", "de"],
+    defaultLocale: "fr",
+  },
+];
